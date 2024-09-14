e9ddbcb828fdb4a250c1237c4a3407ab29461342
baderalb <146772548+baderalb@users.noreply.github.com>
2024-09-14 12:30:52 +0300

    refactor: Improve case-insensitive city check and warning message for Washington dataset
    Case-insensitive check: city.lower() ensures that the comparison works regardless of whether the user types "Washington" in upper or lower case.
    Slight rephrasing: The message is slightly reworded for better clarity.

--- bikeshare.py
+++ bikeshare.py
@@ -74,8 +74,9 @@ def load_data(city, month, day):
     Returns: df (Pandas DataFrame)
     """
     print("\nFilters applied: [{}]".format(", ".join([city, month, day]).center(78, '*')))
-    if city == 'washington':
-        print("Warning: Washington dataset lacks 'Gender' and 'Birth Year' information.")
+    # added an if condtional
+    if city.lower() == 'washington':
+       print("Warning: Washington dataset lacks 'Gender' and 'Birth Year' information.")
 
     df = pd.read_csv(CITY_DATA[city])
     df['Start Time'] = pd.to_datetime(df['Start Time'])
